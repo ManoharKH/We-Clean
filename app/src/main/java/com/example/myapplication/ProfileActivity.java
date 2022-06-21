4960a321167b403378ed6cd9c54458d876050caf
niteeshmurthy bhat <niteeshn158227@gmil.com>
2022-06-21 19:57:23 +0530

    Profile page

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,28 +1,91 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText name, address;
+    private TextView txtusername,txtmob,txtcategory,txtnumberOfComaints;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseAuth mAuth;
+    int numberOfComplaints = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        name = findViewById(R.id.name);
-        address = findViewById(R.id.address);
-
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        txtusername = (TextView) findViewById(R.id.txt_username);
+        txtmob = (TextView) findViewById(R.id.txt_mobilenumber);
+        txtcategory = (TextView) findViewById(R.id.txt_category);
+        txtnumberOfComaints = (TextView) findViewById(R.id.txt_noofcomplaint);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        mAuth = FirebaseAuth.getInstance();
 
+        getUserDetails();
+        countUserComplaints();
+    }
 
+    private void getUserDetails() {
+        String userID = mAuth.getCurrentUser().getUid();
+
+        databaseReference.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // find user with userID
+                DataSnapshot currentUser = snapshot.child(userID);
+
+                txtusername.setText(currentUser.child("username").getValue(String.class));
+                txtmob.setText(currentUser.child("mobile").getValue(String.class));
+                txtcategory.setText(currentUser.child("category").getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void countUserComplaints() {
+        String userID = mAuth.getCurrentUser().getUid();
+
+        databaseReference.child("Complaints").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot complaint : snapshot.getChildren()){
+                    String complaintUserID = complaint.child("userID").getValue(String.class);
+                    if (complaintUserID.equals(userID)){
+                        numberOfComplaints += 1;
+                    }
+                }
+                txtnumberOfComaints.setText("" + numberOfComplaints);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
